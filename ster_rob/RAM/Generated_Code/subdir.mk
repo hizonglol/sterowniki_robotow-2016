8885248e23dba7fcd3e40451a341b4759a9edc79
hizonglol <209193@student.pwr.edu.pl>
2016-04-04 23:46:15 +0200

    Skasowanie starego folderu. Dodanie ster_rob
    ster_rob jest folderem z projektem. Stary był zły

---
+++ ster_rob/RAM/Generated_Code/subdir.mk
@@ -0,0 +1,189 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+-include ../makefile.local
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS_QUOTED += \
+"../Generated_Code/Cpu.c" \
+"../Generated_Code/PE_LDD.c" \
+"../Generated_Code/TU1.c" \
+"../Generated_Code/TU2.c" \
+"../Generated_Code/TU4.c" \
+"../Generated_Code/Vectors.c" \
+"../Generated_Code/dioda_b.c" \
+"../Generated_Code/dioda_g.c" \
+"../Generated_Code/dioda_r.c" \
+"../Generated_Code/periodyczne.c" \
+"../Generated_Code/serwo.c" \
+
+C_SRCS += \
+../Generated_Code/Cpu.c \
+../Generated_Code/PE_LDD.c \
+../Generated_Code/TU1.c \
+../Generated_Code/TU2.c \
+../Generated_Code/TU4.c \
+../Generated_Code/Vectors.c \
+../Generated_Code/dioda_b.c \
+../Generated_Code/dioda_g.c \
+../Generated_Code/dioda_r.c \
+../Generated_Code/periodyczne.c \
+../Generated_Code/serwo.c \
+
+OBJS += \
+./Generated_Code/Cpu.o \
+./Generated_Code/PE_LDD.o \
+./Generated_Code/TU1.o \
+./Generated_Code/TU2.o \
+./Generated_Code/TU4.o \
+./Generated_Code/Vectors.o \
+./Generated_Code/dioda_b.o \
+./Generated_Code/dioda_g.o \
+./Generated_Code/dioda_r.o \
+./Generated_Code/periodyczne.o \
+./Generated_Code/serwo.o \
+
+C_DEPS += \
+./Generated_Code/Cpu.d \
+./Generated_Code/PE_LDD.d \
+./Generated_Code/TU1.d \
+./Generated_Code/TU2.d \
+./Generated_Code/TU4.d \
+./Generated_Code/Vectors.d \
+./Generated_Code/dioda_b.d \
+./Generated_Code/dioda_g.d \
+./Generated_Code/dioda_r.d \
+./Generated_Code/periodyczne.d \
+./Generated_Code/serwo.d \
+
+OBJS_QUOTED += \
+"./Generated_Code/Cpu.o" \
+"./Generated_Code/PE_LDD.o" \
+"./Generated_Code/TU1.o" \
+"./Generated_Code/TU2.o" \
+"./Generated_Code/TU4.o" \
+"./Generated_Code/Vectors.o" \
+"./Generated_Code/dioda_b.o" \
+"./Generated_Code/dioda_g.o" \
+"./Generated_Code/dioda_r.o" \
+"./Generated_Code/periodyczne.o" \
+"./Generated_Code/serwo.o" \
+
+C_DEPS_QUOTED += \
+"./Generated_Code/Cpu.d" \
+"./Generated_Code/PE_LDD.d" \
+"./Generated_Code/TU1.d" \
+"./Generated_Code/TU2.d" \
+"./Generated_Code/TU4.d" \
+"./Generated_Code/Vectors.d" \
+"./Generated_Code/dioda_b.d" \
+"./Generated_Code/dioda_g.d" \
+"./Generated_Code/dioda_r.d" \
+"./Generated_Code/periodyczne.d" \
+"./Generated_Code/serwo.d" \
+
+OBJS_OS_FORMAT += \
+./Generated_Code/Cpu.o \
+./Generated_Code/PE_LDD.o \
+./Generated_Code/TU1.o \
+./Generated_Code/TU2.o \
+./Generated_Code/TU4.o \
+./Generated_Code/Vectors.o \
+./Generated_Code/dioda_b.o \
+./Generated_Code/dioda_g.o \
+./Generated_Code/dioda_r.o \
+./Generated_Code/periodyczne.o \
+./Generated_Code/serwo.o \
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Generated_Code/Cpu.o: ../Generated_Code/Cpu.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #5 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/Cpu.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/Cpu.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/PE_LDD.o: ../Generated_Code/PE_LDD.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #6 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/PE_LDD.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/PE_LDD.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/TU1.o: ../Generated_Code/TU1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #7 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/TU1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/TU1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/TU2.o: ../Generated_Code/TU2.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #8 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/TU2.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/TU2.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/TU4.o: ../Generated_Code/TU4.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #9 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/TU4.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/TU4.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/Vectors.o: ../Generated_Code/Vectors.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #10 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/Vectors.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/Vectors.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/dioda_b.o: ../Generated_Code/dioda_b.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #11 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/dioda_b.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/dioda_b.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/dioda_g.o: ../Generated_Code/dioda_g.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #12 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/dioda_g.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/dioda_g.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/dioda_r.o: ../Generated_Code/dioda_r.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #13 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/dioda_r.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/dioda_r.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/periodyczne.o: ../Generated_Code/periodyczne.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #14 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/periodyczne.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/periodyczne.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/serwo.o: ../Generated_Code/serwo.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #15 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/serwo.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/serwo.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ ster_rob/Sources/Events.h
@@ -0,0 +1,104 @@
+/* ###################################################################
+**     Filename    : Events.h
+**     Project     : ster_rob
+**     Processor   : MK60FX512VLQ15
+**     Component   : Events
+**     Version     : Driver 01.00
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2016-03-27, 21:36, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         Cpu_OnNMIINT - void Cpu_OnNMIINT(void);
+**
+** ###################################################################*/
+/*!
+** @file Events.h
+** @version 01.00
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+
+#ifndef __Events_H
+#define __Events_H
+/* MODULE Events */
+
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+#include "serwo.h"
+#include "TU1.h"
+#include "periodyczne.h"
+#include "TU2.h"
+#include "dioda_b.h"
+#include "TU4.h"
+#include "dioda_g.h"
+#include "dioda_r.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*
+** ===================================================================
+**     Event       :  Cpu_OnNMIINT (module Events)
+**
+**     Component   :  Cpu [MK60FN1M0LQ15]
+*/
+/*!
+**     @brief
+**         This event is called when the Non maskable interrupt had
+**         occurred. This event is automatically enabled when the [NMI
+**         interrupt] property is set to 'Enabled'.
+*/
+/* ===================================================================*/
+void Cpu_OnNMIINT(void);
+
+
+/*
+** ===================================================================
+**     Event       :  periodyczne_OnInterrupt (module Events)
+**
+**     Component   :  periodyczne [TimerInt_LDD]
+*/
+/*!
+**     @brief
+**         Called if periodic event occur. Component and OnInterrupt
+**         event must be enabled. See [SetEventMask] and [GetEventMask]
+**         methods. This event is available only if a [Interrupt
+**         service/event] is enabled.
+**     @param
+**         UserDataPtr     - Pointer to the user or
+**                           RTOS specific data. The pointer passed as
+**                           the parameter of Init method.
+*/
+/* ===================================================================*/
+void periodyczne_OnInterrupt(LDD_TUserData *UserDataPtr);
+
+/* END Events */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif 
+
+#endif 
+/* ifndef __Events_H*/
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
